6e4a824b6bcbb25a7a114bb045b37904aeca2519
Charlie Jiang <cqjjjzr@126.com>
2019-07-17 15:25:04 +0800

    Shift to CharlieJiang's CoolQPushFromWS backend

--- index.js
+++ index.js
@@ -1,63 +1,49 @@
-var config={
-    "BLDFIntervalDelay": 2000,
-    "BLDFRegex": "(?<=[“【]).*(?=[】”])",
-    "BLDFIgnore": "[【】]",
-    "BLTFUrlPrefix": "https://api.vtb.wiki/webapi/message/",
-    "BLTFUrlId":"759280052",
-    "BLTFUrlSuffix": "/history?filter=%E3%80%90&text=true&client=Fly_snow",
+var config = {
+    "BLDFIntervalDelay": 3000,
+    "BLDFRegex": "【(.+?)】",
+
+    "CJId": "ddcenter",
+    "CJUrlPrefix": "http://127.0.0.1:10101/"
 }
 
+var lastTime = '0';
 
 $(function () {
-    var lock=false;
-    setInterval(function () {
-        var BLDFReg = new RegExp(config.BLDFRegex);
-        var BLDFIgnore = new RegExp(config.BLDFIgnore);
-        lock=true;
+    var lock = false;
+    var BLDFReg = new RegExp(config.BLDFRegex, 'g');
+    function update() {
+        if (lock) return;
+        lock = true;
         $.ajax({
             method: "GET",
-            cache:false,
-            url: config.BLTFUrlPrefix+config.BLTFUrlId+config.BLTFUrlSuffix+"&t="+(Date.parse(new Date())),
-            success: function(data) {
-                var tsget=[];
-                tsget=data.trim().split("\n");
-                for(var i=0;i<tsget.length;i++){
-                    var ss="";
-                    while(ss!=tsget[i]){
-                        ss=tsget[i];
-                        tsget[i]=tsget[i].replace(/<.*>/g,"");
-                    }
-                    var matchls=tsget[i].match(BLDFReg);
-                    if(matchls!=null)
-                    for(var j=0;j<matchls.length;j++){
-                        tsget[i]=tsget[i].replace(matchls[j],"<span class=\"translation\">"+matchls[j]+"</span>")
-                    }
-                    ss="";
-                    while(ss!=tsget[i]){
-                        ss=tsget[i];
-                        tsget[i]=tsget[i].replace(BLDFIgnore,"");
-                    }
+            cache: true,
+            url: config.CJUrlPrefix + "?args=" + config.CJId + '|' + lastTime,
+            success: function (data) {
+                var tsget = data.trim().split("\n");
+                lastTime = tsget[0];
+                tsget.shift();
+                tsget.slice().reverse().forEach(function (line, index) {
+                    var lineProc = line.replace(/<.*>/g, "");
+                    lineProc = filterXSS(lineProc);
+                    var matches = lineProc.match(BLDFReg);
+                    if (matches != null)
+                        matches.forEach(function (match) {
+                            lineProc = lineProc.replace(match, "<span class=\"translation\">" + match.replace("【", "").replace("】", "") + "</span>");
+                        });
 
-                }
-                var tsexist=[]
-                $(".SubtitleTextBody p").each(function(i,obj){
-                    if($(obj).attr("style")==null || $(obj).attr("style")=="")tsexist.push($(obj).html());
-                });
-                var pget=tsget.length-1;
-                var pexist=0;
-                while(pget>=0&&tsget[pget]!=tsexist[pexist])pget--;
-                for(var i=pget+1;i<tsget.length;i++){
-                    $('.SubtitleTextBody').prepend("<p class='new'>" + tsget[i] + "</p>");
+                    $('.SubtitleTextBody').prepend("<p class='new'>" + lineProc + "</p>");
                     setTimeout(function () {
                         $('.new').removeClass('new');
-                    },1);
-                }
-
-
-            },
-            complete:function () {
-                lock=false;
+                    }, 1);
+                })
+            }, complete: function () {
+                lock = false;
             }
         })
-    },config.BLDFIntervalDelay);
+    }
+
+    update();
+    setInterval(function () {
+        update();
+    }, config.BLDFIntervalDelay);
 });
